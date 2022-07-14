8f5a096ad025ed1dc06eb5f8dc4a3074274933ed
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-07-14 17:29:30 +0200

    fix: mensajes de error

--- balance/forms.py
+++ balance/forms.py
@@ -9,9 +9,10 @@ class MovimientosForm(FlaskForm):
         message="Debes introducir una fecha")])
     concepto = StringField(
         "Concepto", validators=[
-            DataRequired("Debes escribir un concepto del movimiento"),
+            DataRequired(message="Debes escribir un concepto del movimiento"),
             Length(min=3, max=25, message="Debe tener entre 3 y 25 caracteres")])
     tipo = RadioField(choices=[("I", "Ingreso"), ("G", "Gasto")])
-    cantidad = FloatField("Cantidad")
+    cantidad = FloatField("Cantidad", validators=[DataRequired(
+        message="Debes indicar una cantidad válida")])
 
     submit = SubmitField("Guardar", render_kw={"class": "green-button"})
